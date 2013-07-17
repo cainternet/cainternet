001257b19bc8ad8b0da53800a785da9ae8cdef31
Austen Collins <austencollins@gmail.com>
2013-07-17 19:19:44 +0200

    Changes

--- app/assets/javascripts/views/service_results_view.js
+++ app/assets/javascripts/views/service_results_view.js
@@ -17,19 +17,19 @@ CaliforniaInternet.Views.ServiceResultsView = Backbone.View.extend({
     openOrderBox: function() {
       		var servicesCount = 0
       		var servicesArray = []
-      		$( ".selected" ).each(function() {
-      			var serviceName = $(this).children('span').text()
+      		$( "input:checked" ).each(function() {
+      			var serviceName = $(this).data("service");
       		    servicesArray.push(serviceName)
 			    servicesCount = servicesCount + 1
 			});
 			console.log(servicesArray);
 			if (servicesCount > 0) {
-	      		var city = $('#cta-city').val();
-	    		var orderBoxView = new CaliforniaInternet.Views.OrderBoxView({ city: city, services: servicesArray })
-	      		$('#service-areas-container').html(orderBoxView.render().$el);
-	      	} else {
-	      		alert("Please Select A Service You Are Interested In");
-	      	};
+                var city = $('#cta-city').val();
+		    	var orderBoxView = new CaliforniaInternet.Views.OrderBoxView({ city: city, services: servicesArray })
+		      	$('#service-areas-container').html(orderBoxView.render().$el);
+		    } else {
+		      	alert("Please Select A Service You Are Interested In");
+		    };
     },
 
     openServiceModal: function(e) {
